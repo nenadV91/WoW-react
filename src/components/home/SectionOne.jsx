0dc65dad5716a351daa056bba52e3bd75cefcf9b
nenadV91 <nenadwebdevelop@gmail.com>
2018-05-09 00:01:18 +0200

    GH pages.

--- src/components/home/SectionOne.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default () => (
-	<section className="d-flex align-items-center justify-content-center">
-		<h1 className="display-4 wow bounceIn" data-wow-delay="0.3s">Section 1</h1>
-	</section>
-)
