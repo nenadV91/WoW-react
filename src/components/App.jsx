41aa6c83fe00eaba0e6a97bbc22bcee2a06da917
nenadV91 <nenadwebdevelop@gmail.com>
2018-05-09 00:14:46 +0200

    Updated Router.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import {Switch, BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {Switch, HashRouter as Router, Route, Link} from 'react-router-dom';
 import {Navbar, Nav, NavItem, NavLink} from 'reactstrap'
 
 import Home from './Home';
 import About from './About';
 
 const App = props => {
-	console.log(process.env.PUBLIC_URL);
-	console.log(process.env)
-	
-	return <Router basename={process.env.PUBLIC_URL}>
+	return <Router>
 		<div>
 			<Navbar fixed="top" color="dark" dark>
 				<Nav className="ml-auto">
